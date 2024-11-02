d85fcddc435d9dccf7e9bc6bc0911eaee9bcdfbd
joshua <joshua@gmail.com>
2024-11-02 21:21:16 +0800

    adding requirement to the config to database in login

--- index.php
+++ index.php
@@ -6,4 +6,10 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: dashboard.php");
     exit;
 }
+
+require_once "config.php";
+ 
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
+ 
 ?>
