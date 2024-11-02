d0588b50cb4cf287e5f20db46d132d4332472642
joshua <joshua@gmail.com>
2024-11-02 21:22:13 +0800

    looking foe the user pass and username

--- index.php
+++ index.php
@@ -12,4 +12,18 @@ require_once "config.php";
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
  
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
+    }
+    
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
 ?>
