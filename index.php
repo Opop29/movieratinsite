5729727026c7f66b6029221edfa2a4262feb168c
joshua <joshua@gmail.com>
2024-11-02 21:23:17 +0800

    done for the php proces in index

--- index.php
+++ index.php
@@ -26,4 +26,51 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     } else{
         $password = trim($_POST["password"]);
     }
+    
+    if(empty($username_err) && empty($password_err)){
+       
+        $sql = "SELECT id, username, password FROM users WHERE username = :username";
+        
+        if($stmt = $pdo->prepare($sql)){
+           
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            
+            $param_username = trim($_POST["username"]);
+            
+            if($stmt->execute()){
+                
+                if($stmt->rowCount() == 1){
+                    if($row = $stmt->fetch()){
+                        $id = $row["id"];
+                        $username = $row["username"];
+                        $hashed_password = $row["password"];
+                        if(password_verify($password, $hashed_password)){
+                            
+                            session_start();
+                            
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                           
+                            header("location:dashboard.php");
+                        } else{
+                           
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else{
+                   
+                    $login_err = "Invalid username or password.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            unset($stmt);
+        }
+    }
+   
+    unset($pdo);
+}
 ?>
